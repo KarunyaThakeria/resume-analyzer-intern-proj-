b74b695f5da2dea0dda8bac2400b4f3d8d8089a1
KarunyaThakeria <karunya16t@gmail.com>
2025-10-27 15:21:57 +0530

    final fin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// ⚠️ Replace "resume-analyser-intern-project-2025" with your GitHub repo name
 export default defineConfig({
-    base: "/resume-analyzer-intern-proj-/",
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
